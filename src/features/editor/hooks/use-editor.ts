6ad127eb5e78d3e4eb76cc927a79d0013d62c0c6
MyDragon168 <cambodiagames3@gmail.com>
2024-09-14 22:25:21 +0700

    add custome prototype on object(shape) to canvas

--- src/features/editor/hooks/use-editor.ts
+++ src/features/editor/hooks/use-editor.ts
@@ -9,6 +9,17 @@ export const useEditor = () => {
       initialCanvas: fabric.Canvas;
       initialContainer: HTMLDivElement;
     }) => {
+      //custome prototype of object(shape)
+      fabric.Object.prototype.set({
+        cornerColor: "#fff",
+        cornerStyle: "circle",
+        borderColor: "#3b82f6",
+        borderScaleFactor: 1.5,
+        transparentCorners: false,
+        borderOpacityWhenMoving: 1,
+        cornerStrokeColor: "#3b82f6",
+      });
+
       const initialWorkspace = new fabric.Rect({
         width: 900,
         height: 1200,
