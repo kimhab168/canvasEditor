67fcf30cc5dc68711ad5388501b6b25b62a3cc3b
MyDragon168 <cambodiagames3@gmail.com>
2024-09-15 22:33:10 +0700

    end of resonsive updated

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef } from "react";
 import { Navbar } from "./navbar";
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { Footer } from "./footer";
 const Editor = () => {
   const { init } = useEditor();
 
@@ -26,9 +27,13 @@ const Editor = () => {
         <Sidebar />
         <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
           <Toolbar />
-          <div className="flex-1 h-full bg-muted" ref={containerRef}>
+          <div
+            className="flex-1 h-[calc(100%-124px)] bg-muted"
+            ref={containerRef}
+          >
             <canvas ref={canvasRef} />
           </div>
+          <Footer />
         </main>
       </div>
     </div>
---
+++ src/features/editor/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="h-[52px] border-t bg-white w-full flex items-center overflow-auto z-[49] p-2 gap-x-1 shrink-0 px-4 flex-row-reverse">
+      footer
+    </footer>
+  );
+};
